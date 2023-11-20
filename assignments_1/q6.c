38d5249a5e1901f49c5d6c6c93a52100f619ad16
Raja <rdas6313@gmail.com>
2023-11-20 13:35:46 +0530

    solved question 6 of assignment 1

---
+++ assignments_1/q6.c
@@ -0,0 +1,31 @@
+/*
+Program to GCD of two number using factorization and euclid method.
+written by Raja Das ( Roll 52 )
+*/
+
+#include<stdio.h>
+
+int gcd_fact(int a,int b){
+	int min = a < b ? a : b;
+	int i;
+	for(i=min;i>=1;i--){
+		if(a % i == 0 && b % i == 0)
+			return i;
+	}
+	return 1;
+}
+
+int gcd_euclid(int a,int b){
+	if(b == 0)
+		return a;
+	return gcd_fact(b,a%b);
+}
+
+int main(){
+	int a,b;
+	printf("Enter two number: ");
+	scanf("%d %d",&a,&b);
+	printf("GCD of %d and %d (Factorization method): %d\n",a,b,gcd_fact(a,b));
+	printf("GCD of %d and %d (Euclid method): %d\n",a,b,gcd_euclid(a,b));
+	return 0;
+}
