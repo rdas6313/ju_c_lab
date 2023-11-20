99f7fce42886ef1aebf4494fc687b0a5675b1363
Raja <rdas6313@gmail.com>
2023-11-20 19:33:03 +0530

    fixed bug of question 3 of assignment 1

--- assignments_1/q3.c
+++ assignments_1/q3.c
@@ -4,25 +4,16 @@ numbers to the right of X. Total Room number is given as input.
 written by Raja Das ( Roll 52 )
 */
 #include<stdio.h>
+#include<math.h>
 int main(){
 	printf("Total rooms: ");
 	int total_rooms;
 	scanf("%d",&total_rooms);
-	int start = 1;
-	int end = total_rooms;
-	long long int sum = (total_rooms * ((long long)total_rooms + 1))/2;
-	while(start <= end){
-		int x =	(start+end)/2;
-		long long int sumx = (x * ((long long)x+1))/2;
-		
-		if(2 * sumx == sum){
-			printf("Room no X is: %d\n",x);
-			return 0;	
-		}else if(sumx < (sum-sumx)){
-			start = x + 1;
-		}else
-			end = x - 1;
-		
+	int tmp = (total_rooms*(total_rooms+1))/2;
+	int ans = sqrt(tmp);
+	if(ans * ans == tmp){
+		printf("Room x is: %d\n",ans);
+		return 0;
 	}
 	printf("No Room x found for given total room %d\n",total_rooms);
 	return 0;
