6a81ac26eab22befd96b80f11266a743dc6008bf
Raja <rdas6313@gmail.com>
2023-11-20 20:02:37 +0530

    Added some compilation info

--- assignments_1/q3.c
+++ assignments_1/q3.c
@@ -1,6 +1,6 @@
 /*
 Program to find X where the sum of the room numbers left to X is equal to the sum of the room
-numbers to the right of X. Total Room number is given as input.
+numbers to the right of X. Total Room number is given as input. (use -lm option with gcc to link math library.)
 written by Raja Das ( Roll 52 )
 */
 #include<stdio.h>
