d0e251d7fe41eb9baf8e975a988a8d64763e9081
Raja Das <rdas6313@gmail.com>
2024-01-02 21:02:28 +0530

    Solved question 3 of assignments 4

---
+++ assignments_4/q3.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+using namespace std;
+
+class Number_List{
+	int nums[100000];
+	int length;
+	public:
+		void create_array(){
+			int n;
+			cout << "No of elements in array: ";
+			cin >> n;
+			length = n;
+			cout << "Elements of array: ";
+			for(int i=0;i<n;i++)
+				cin >> nums[i];
+		}
+
+		void sort(){
+			int n = length;
+			for(int i=0;i<n-1;i++){
+				for(int j=i+1;j<n;j++){
+					if(nums[i] > nums[j]){
+						int tmp = nums[i];
+						nums[i] = nums[j];
+						nums[j] = tmp;
+					}
+				}
+			}
+			cout << "sorted array: ";
+			for(int i=0;i<n;i++)
+				cout << nums[i] << " ";
+			cout << endl;
+		}
+		void maximum(){
+			int n = length;
+			int max = nums[0];
+			for(int i=0;i<n;i++){
+				if(max < nums[i])
+					max = nums[i];
+			}
+			cout << "Maximum: " << max << endl;
+		}
+		void minimum(){
+			int n = length;
+			int min = nums[0];
+			for(int i=0;i<n;i++){
+				if(min > nums[i])
+					min = nums[i];
+			}
+			cout << "Minimum: " << min << endl;
+
+		}
+};
+
+int main(){
+	Number_List list;
+	list.create_array();
+	list.sort();
+	list.maximum();
+	list.minimum();
+	return 0;
+}
